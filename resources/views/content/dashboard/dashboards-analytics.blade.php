87d7fbc7dabcab11dd834e391027733587d051a1
Gabriel Milani <contato@milanitechnology.com.br>
2023-04-29 15:09:40 -0300

    initial commit

---
+++ resources/views/content/dashboard/dashboards-analytics.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts/contentNavbarLayout')
+
+@section('title', 'Meu Perfil - Pwr Saúde+')
+
+@section('vendor-style')
+<link rel="stylesheet" href="{{asset('assets/vendor/libs/apex-charts/apex-charts.css')}}">
+@endsection
+
+@section('vendor-script')
+<script src="{{asset('assets/vendor/libs/apex-charts/apexcharts.js')}}"></script>
+@endsection
+
+@section('page-script')
+<script src="{{asset('assets/js/dashboards-analytics.js')}}"></script>
+@endsection
+
+@section('content')
+<div class="row">
+  <div class="col-lg-8 mb-4 order-0">
+    <div class="card">
+      <div class="d-flex align-items-end row">
+        <div class="col-sm-7">
+          <div class="card-body">
+            <h5 class="card-title text-primary">Olá {{config('variables.templateName')}}! 🎉</h5>
+            <p class="mb-4">Seja bem-vindo a plataforma da Pwr Saúde!</p>
+          </div>
+        </div>
+        <div class="col-sm-5 text-center text-sm-left">
+          <div class="card-body pb-0 px-0 px-md-4">
+            <img src="{{asset('assets/img/illustrations/man-with-laptop-light.png')}}" height="140" alt="View Badge User" data-app-dark-img="illustrations/man-with-laptop-dark.png" data-app-light-img="illustrations/man-with-laptop-light.png">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
